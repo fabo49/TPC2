2349ee63e860d63268ea1f7fb02e157ab026b59c
fabo49 <farodriguez.49@gmail.com>
2015-04-07 14:15:22 -0600

    Creación matriz de cada proceso

--- main.cpp
+++ main.cpp
@@ -12,6 +12,7 @@
 #include "mpi.h"
 #include <iostream>
 #include <stdio.h>
+#include <time.h>
 
 using namespace std;
 int main(int argc, char *argv[])
@@ -22,6 +23,7 @@ int main(int argc, char *argv[])
     int numCols;                        /* El numero de columnas que va a tener cada matriz. */
     int numFils;                        /* El numero de filas que va a tener cada matriz (tiene que ser par). */
     int** mat;                          /* La matriz de enteros de cada proceso. */
+    int* datos;                         /* Es donde van a estar los datos de la matriz. */
     pthread_t hilo1;                    /* Estos dos son los hilos que va a crear cada proceso. */
     pthread_t hilo2;
 
@@ -31,6 +33,7 @@ int main(int argc, char *argv[])
     MPI_Comm_rank(MPI_COMM_WORLD, &myID);       /* Almacena en myID el identificador del proceso que hizo el llamado. */
 
     if(myID == 0){  //soy el proceso padre
+        srand(time(NULL));  //inicializa la semilla
         cout<<"Indique cuantas columnas quiere que tengan las matrices: ";
         cin>>numCols;
         bool valid = false;
@@ -46,7 +49,15 @@ int main(int argc, char *argv[])
         }
     }
 
+    datos = new int[numFils*numCols];       /* Va a crear un vector grande que luego lo va a partir para poner en la matriz. */
+    mat = new int*[numFils];
+    for(int i=0; i< numFils*numCols; ++i){  //voy a llenar el vector de numeros aleatorios
+        datos[i] = (rand()%100+1)-(rand()%100+1);
+    }
 
+    for(int i=0; i<numFils; ++i){
+        mat[i] = &(datos[i*numCols]);
+    }
 
 
     return 0;

d5a1dad8890b7cb05b4c30f0e8dd17fc636f4418
fabo49 <farodriguez.49@gmail.com>
2015-04-07 10:57:54 -0600

    Pide al usuario las dimensiones de la matriz

--- main.cpp
+++ main.cpp
@@ -13,8 +13,40 @@
 #include <iostream>
 #include <stdio.h>
 
+using namespace std;
 int main(int argc, char *argv[])
 {
+
+    int myID;                           /* El identificador de cada proceso. */
+    int numProcs;                       /* Cuantos procesos pidio el usuario. */
+    int numCols;                        /* El numero de columnas que va a tener cada matriz. */
+    int numFils;                        /* El numero de filas que va a tener cada matriz (tiene que ser par). */
+    int** mat;                          /* La matriz de enteros de cada proceso. */
+    pthread_t hilo1;                    /* Estos dos son los hilos que va a crear cada proceso. */
+    pthread_t hilo2;
+
+    MPI_Init(&argc, &argv);             /* Inicializa MPI*/
+
+    MPI_Comm_size(MPI_COMM_WORLD, &numProcs);   /* Pone en numProcs el numero que indico el usuario con la cantidad de procesos a crear. */
+    MPI_Comm_rank(MPI_COMM_WORLD, &myID);       /* Almacena en myID el identificador del proceso que hizo el llamado. */
+
+    if(myID == 0){  //soy el proceso padre
+        cout<<"Indique cuantas columnas quiere que tengan las matrices: ";
+        cin>>numCols;
+        bool valid = false;
+        while(valid == false){
+            cout<<"\nIngrese la cantidad de filas que quiere que tengan las matrices, tiene que ser un numero par: ";
+            cin>>numFils;
+            if(numFils%2 == 0){ //es un numero par
+                valid = true;
+            }else{
+                cout<<"\nERROR: la cantidad de filas ingresada no es un numero par"<<endl;
+                cout<<"------------------------------------------------------------"<<endl;
+            }
+        }
+    }
+
+
 
 
     return 0;

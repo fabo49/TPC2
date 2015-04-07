d73fc519248fb11a750b6a75acd1f13f01d70455
fabo49 <farodriguez.49@gmail.com>
2015-04-07 10:31:16 -0600

    Primer Commit
    Se crea el archivo donde va a estar el programa

---
+++ main.cpp
@@ -0,0 +1,21 @@
+/**
+  * Universidad de Costa Rica
+  * Escuela de Ciencias de la Computacion e Informatica
+  * Arquitectura de Computadoras
+  * Tarea Corta 2: Creacion de Hilos
+  * @author Fabian Rodriguez Obando
+  * @author Jose Pablo Urena
+  */
+
+#include <QCoreApplication>
+#include <pthread.h>
+#include "mpi.h"
+#include <iostream>
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+
+
+    return 0;
+}
